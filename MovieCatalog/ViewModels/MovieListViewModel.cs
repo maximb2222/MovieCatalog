672bf5c3764e122f401bbf98a86dc1a0d9e946a2
maximb2222 <198604004+maximb2222@users.noreply.github.com>
2026-04-01 02:34:53 +0300

    Добавьте файлы проекта.

---
+++ MovieCatalog/ViewModels/MovieListViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+using MovieCatalog.Models;
+
+namespace MovieCatalog.ViewModels;
+
+public class MovieListViewModel : BaseViewModel
+{
+    private MovieViewModel? _selectedMovie;
+
+    public ObservableCollection<MovieViewModel> Movies { get; } = new();
+
+    public MovieViewModel? SelectedMovie
+    {
+        get => _selectedMovie;
+        set => SetProperty(ref _selectedMovie, value);
+    }
+
+    public MovieListViewModel()
+    {
+        LoadMovies();
+    }
+
+    private void LoadMovies()
+    {
+        var moviesList = new List<Movie>
+        {
+            new Movie { Title = "Матрица", Year = 1999, Director = "Вачовски", Genre = "Фантастика" },
+            new Movie { Title = "Начало", Year = 2010, Director = "Нолан", Genre = "Триллер" },
+            new Movie { Title = "Криминальное чтиво", Year = 1994, Director = "Тарантино", Genre = "Криминал" },
+            new Movie { Title = "Побег из Шоушенка", Year = 1994, Director = "Дарабонт", Genre = "Драма" }
+        };
+
+        foreach (var movie in moviesList)
+        {
+            Movies.Add(new MovieViewModel(movie));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+<img width="1282" height="570" alt="image" src="https://github.com/user-attachments/assets/77d08df9-79ab-4ec4-9eed-a0d2915f8081" />
+<img width="1402" height="482" alt="image" src="https://github.com/user-attachments/assets/46498c2c-ef0f-4a66-a2ec-2d107bdd3ec7" />
+<img width="405" height="938" alt="image" src="https://github.com/user-attachments/assets/ad34f601-8c30-465e-a829-bebf5982cddd" />
+<img width="413" height="941" alt="image" src="https://github.com/user-attachments/assets/447c6e1e-baa5-4a74-b2dd-961e0f344584" />
